f4a746a27af34339c26cb8f1d700f49902d20ee1
Joshua Barnett <joecoolatbyu@ldschurch.org>
2018-08-31 13:47:38 -0600

    Initial Commit of Cat Age App

--- Cat Years/ViewController.swift
+++ Cat Years/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var ageText: UITextField!
+    @IBOutlet weak var toggleSwitch: UISwitch!
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        if let age = ageText.text {
+            if let catAge = Int(age){
+                let ccAge:Int
+                if (catAge<=2){
+                    ccAge = catAge*12
+                } else if (toggleSwitch.isOn){
+                    ccAge = 24 + (catAge-2) * 8
+                } else {
+                    ccAge = 24 + (catAge-2) * 4
+                }
+                ageLabel.text = "Your cat is " + String(ccAge) + " in cat years"
+            }
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
